df28f18d6b25141d04b4d0c92d6ed104804f577d
Manas Pratim Das <110994286+manas-pr@users.noreply.github.com>
2025-02-23 22:45:55 +0530

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import streamlit as st
+import os
+
+def decrypt_image(image):
+    decrypted_msg = ""
+    m, n, z = 0, 0, 0
+
+    while True:
+        char = c.get(image[n, m, z], "")
+        if char == "\0":  # Stop at termination character
+            break
+        decrypted_msg += char
+        n = (n + 1) % image.shape[0]
+        m = (m + 1) % image.shape[1]
+        z = (z + 1) % 3
+
+    return decrypted_msg
