4b16e598e4ada8913bc74e45b149af95ca2c6262
Manas Pratim Das <110994286+manas-pr@users.noreply.github.com>
2025-02-24 01:34:07 +0530

    encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,17 +1,28 @@
-import numpy as np
 import cv2
+import numpy as np
+import hashlib
+from Crypto.Cipher import AES
+import base64
 
-def encrypt_image(image, message, password=None):
-    """Encrypts a message into an image using pixel manipulation."""
-    if password:
-        message = password + message  # Simple way to integrate password
-    message += "\0"  # End-of-message marker
+def encrypt_image(image, message, password):
+    # Hash the password
+    password_hash = hashlib.sha256(password.encode()).digest()
+    
+    # Pad the message to be AES block-size compatible
+    def pad(text):
+        return text + (16 - len(text) % 16) * chr(16 - len(text) % 16)
+    
+    padded_message = pad(message)
+
+    # Encrypt message using AES
+    cipher = AES.new(password_hash, AES.MODE_ECB)
+    encrypted_msg = base64.b64encode(cipher.encrypt(padded_message.encode())).decode()
 
     img = image.copy()
     m, n, z = 0, 0, 0
 
-    for char in message:
-        img[n, m, z] = ord(char)  # Convert character to ASCII value
+    for char in encrypted_msg:
+        img[n, m, z] = ord(char)
         n = (n + 1) % img.shape[0]
         m = (m + 1) % img.shape[1]
         z = (z + 1) % 3
