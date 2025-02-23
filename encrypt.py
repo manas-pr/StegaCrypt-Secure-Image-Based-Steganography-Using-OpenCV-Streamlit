fb5bbfc7bcdcc6d8d67aa9742b5bc5dd829d09b5
Manas Pratim Das <110994286+manas-pr@users.noreply.github.com>
2025-02-24 02:17:14 +0530

    encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -24,22 +24,17 @@ def encrypt_message(message, password):
 
 # Function to embed message into an image
 def encrypt_image(image, message, password):
+    encrypted_message = encrypt_message(message, password)  # Encrypt message
+    encrypted_message += "\0"  # Ensure termination
+
     img = image.copy()
-    
-    # Encrypt the message
-    encrypted_message = encrypt_message(message, password)
-    message_length = len(encrypted_message)
+    m, n, z = 0, 0, 0
 
-    # Store message length in the first 8 pixels
-    for i in range(8):
-        img[i, 0, 0] = (message_length >> (i * 8)) & 255  # Store length in red channel
-
-    # Embed message characters in the image
-    m, n, z = 8, 0, 0  # Start from pixel index 8 to avoid overwriting length
     for char in encrypted_message:
-        img[n, m, z] = ord(char)  # Store ASCII value of character
+        img[n, m, z] = ord(char)  # Store ASCII values
         n = (n + 1) % img.shape[0]
         m = (m + 1) % img.shape[1]
         z = (z + 1) % 3
 
     return img
+
