83b97186097cf2c117d7637356401b178d08dcfd
Manas Pratim Das <110994286+manas-pr@users.noreply.github.com>
2025-02-24 02:08:21 +0530

    decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -3,29 +3,13 @@ import numpy as np
 import streamlit as st
 import os
 
-# Define character-to-value mapping
-d = {chr(i): i for i in range(255)}
-c = {i: chr(i) for i in range(255)}
-
-def encrypt_image(image, message):
-    img = image.copy()
-    m, n, z = 0, 0, 0
-
-    for char in message:
-        img[n, m, z] = d[char]
-        n = (n + 1) % img.shape[0]
-        m = (m + 1) % img.shape[1]
-        z = (z + 1) % 3
-
-    return img
-
 def decrypt_image(image):
     decrypted_msg = ""
     m, n, z = 0, 0, 0
 
     while True:
         char = c.get(image[n, m, z], "")
-        if char == "":  # Stop when an invalid character is encountered
+        if char == "\0":  # Stop at termination character
             break
         decrypted_msg += char
         n = (n + 1) % image.shape[0]
