c11c5d7497fbd8dbf02cf8b07447af817b90fc12
Manas Pratim Das <110994286+manas-pr@users.noreply.github.com>
2025-02-24 00:34:36 +0530

    decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,19 +1,26 @@
-import cv2
 import numpy as np
-import streamlit as st
-import os
+import cv2
 
-def decrypt_image(image):
+def decrypt_image(image, password=None):
+    """Decrypts a message from an image, verifying password if provided."""
     decrypted_msg = ""
     m, n, z = 0, 0, 0
 
     while True:
-        char = c.get(image[n, m, z], "")
-        if char == "\0":  # Stop at termination character
+        char = chr(image[n, m, z])  # Convert pixel value back to character
+        if char == "\0":  # End of message
             break
         decrypted_msg += char
         n = (n + 1) % image.shape[0]
         m = (m + 1) % image.shape[1]
         z = (z + 1) % 3
 
+    # Verify password if provided
+    if password:
+        if decrypted_msg.startswith(password):
+            return decrypted_msg[len(password):]  # Return message without password
+        else:
+            return "❌ Incorrect password!"
+
     return decrypted_msg
+
