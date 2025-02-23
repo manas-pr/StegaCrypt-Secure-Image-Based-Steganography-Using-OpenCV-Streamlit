41b1c3531b7dd7b1e91799eeae75d507c14408da
Manas Pratim Das <110994286+manas-pr@users.noreply.github.com>
2025-02-24 00:34:14 +0530

    encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,19 +1,17 @@
-import cv2
 import numpy as np
-import streamlit as st
-import os
+import cv2
 
-# Define character-to-value mapping
-d = {chr(i): i for i in range(255)}
-c = {i: chr(i) for i in range(255)}
+def encrypt_image(image, message, password=None):
+    """Encrypts a message into an image using pixel manipulation."""
+    if password:
+        message = password + message  # Simple way to integrate password
+    message += "\0"  # End-of-message marker
 
-def encrypt_image(image, message):
     img = image.copy()
-    message += "\0"  # Add termination character
     m, n, z = 0, 0, 0
 
     for char in message:
-        img[n, m, z] = d[char]
+        img[n, m, z] = ord(char)  # Convert character to ASCII value
         n = (n + 1) % img.shape[0]
         m = (m + 1) % img.shape[1]
         z = (z + 1) % 3
