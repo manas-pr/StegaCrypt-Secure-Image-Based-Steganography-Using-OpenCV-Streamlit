f8392504e31fd2085d20e9ab658b3c6501b94a60
Manas Pratim Das <110994286+manas-pr@users.noreply.github.com>
2025-02-24 13:35:21 +0530

    StegaCrypt.py

--- StegaCrypt.py
+++ StegaCrypt.py
@@ -3,6 +3,43 @@ import numpy as np
 import streamlit as st
 import os
 
+###########################################################################
+
+# Define character-to-value mapping
+d = {chr(i): i for i in range(255)}
+c = {i: chr(i) for i in range(255)}
+
+def encrypt_image(image, message):
+    img = image.copy()
+    message += "\0"  # Add termination character
+    m, n, z = 0, 0, 0
+
+    for char in message:
+        img[n, m, z] = d[char]
+        n = (n + 1) % img.shape[0]
+        m = (m + 1) % img.shape[1]
+        z = (z + 1) % 3
+
+    return img
+####################################################################################
+
+def decrypt_image(image):
+    decrypted_msg = ""
+    m, n, z = 0, 0, 0
+
+    while True:
+        char = c.get(image[n, m, z], "")
+        if char == "\0":  # Stop at termination character
+            break
+        decrypted_msg += char
+        n = (n + 1) % image.shape[0]
+        m = (m + 1) % image.shape[1]
+        z = (z + 1) % 3
+
+    return decrypted_msg
+
+
+###########################################################
 
 # Set background image using CSS
 page_bg_img = """
